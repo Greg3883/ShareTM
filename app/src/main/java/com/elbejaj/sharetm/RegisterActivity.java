5c315e3f517f820ce49f3b0d0648c6a8769b0d68
NayWaYo <onlinegames217@gmail.com>
2017-03-26 19:36:57 +0200

    Ajout user quasi fonctionnel
    il fonctionne, mais faut le faire deux fois, paske ça crash la 1er fois
    :D

--- app/src/main/java/com/elbejaj/sharetm/RegisterActivity.java
+++ app/src/main/java/com/elbejaj/sharetm/RegisterActivity.java
@@ -1,7 +1,7 @@
 package com.elbejaj.sharetm;
 
 /**
- * Created by Baalamor on 25/03/2017.
+ * Created by Valentin on 25/03/2017.
  */
 
 import android.content.Intent;
@@ -15,6 +15,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.ExecutionException;
+
 import butterknife.Bind;
 
 
@@ -23,9 +25,10 @@ public class RegisterActivity extends AppCompatActivity {
 
     /*useless*/
     UtilisateurDAO ud;
-    TextView login_incorrect;
     ApiInterface apiInterface = STMAPI.getClient().create(ApiInterface.class);
 
+
+    @Bind(R.id.input_name) EditText _nameText;
     @Bind(R.id.input_mail) EditText _mailText;
     @Bind(R.id.input_pwd) EditText _pwdText;
     @Bind(R.id.input_confirmpwd) EditText _confirmPwdText;
@@ -40,10 +43,6 @@ public class RegisterActivity extends AppCompatActivity {
 
         Button valider = (Button) findViewById(R.id.btn_valider);
 
-
-        EditText pwd = (EditText) findViewById(R.id.input_pwd);
-        EditText confpwd = (EditText) findViewById(R.id.input_confirmpwd);
-
         valider.setOnClickListener(new View.OnClickListener() {
 
                                             @Override
@@ -56,7 +55,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     }
 
-
+    //test la validité du champ "Nom" (vide ou non)
     public Boolean isEmptyName() {
         EditText name = (EditText) findViewById(R.id.input_name);
         String namestr = name.getText().toString();
@@ -67,6 +66,7 @@ public class RegisterActivity extends AppCompatActivity {
         else return false;
     }
 
+    //test la validité du champ "Adresse e-mail" (vide + syntaxe)
     public Boolean isEmailValide() {
         String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
         EditText mail = (EditText) findViewById(R.id.input_mail);
@@ -82,7 +82,7 @@ public class RegisterActivity extends AppCompatActivity {
         return true;
     }
 
-
+    //test la validité des champs "mot de passe" (vide + différents)
     public Boolean isPasswordValide() {
         EditText pwd = (EditText) findViewById(R.id.input_pwd);
         EditText confpwd = (EditText) findViewById(R.id.input_confirmpwd);
@@ -107,16 +107,51 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
 
-
+    //appelle tous les test sur les champs, puis la fonction d'inscription avec l'API
     public void register() {
-
-
         if (!isEmptyName()) {
             if (isEmailValide()) {
                 if (isPasswordValide()) {
                     Toast.makeText(RegisterActivity.this, "Validé", Toast.LENGTH_SHORT).show();
+                    registerAPI();
                 }
             }
+        }
+
+    }
+
+    //communique avec l'API pour l'inscription (ajout dans la BdD)
+    public void registerAPI() {
+
+        //Récupération du nom, de l'email et du mot de passe
+        EditText nom = (EditText) findViewById(R.id.input_name);
+        EditText mail = (EditText) findViewById(R.id.input_mail);
+        EditText pwd = (EditText) findViewById(R.id.input_pwd);
+        String nomstr = nom.getText().toString();
+        String mailstr = mail.getText().toString();
+        String pwdstr = pwd.getText().toString();
+
+        //Exécution de l'inscription
+        Log.i("test","registerActivity : Je vais exécuter le registerTask");
+        AsyncTask registerTask = new RegisterTask(this,this.ud).execute(nomstr,mailstr,pwdstr);
+        Object aFonctionne = false;
+        try {
+            aFonctionne = registerTask.get();
+            Log.i("test","registerActivity : Résultat du task : "+aFonctionne.toString());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        Boolean registerAFonctionne = (Boolean) aFonctionne;
+
+        //Avant de démarrer le LoginActivity, on vérifie si l'inscription a fonctionné
+        if(registerAFonctionne==true) {
+            Intent i = new Intent (".LOGINACTIVITY");
+            startActivity(i);
+        } else {
+            Toast.makeText(this,"Problème lors de l'inscription. Veuillez réessayer.",Toast.LENGTH_LONG).show();
         }
 
     }
